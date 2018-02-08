237b9d7296b750d98ec42d334d80b6b682663aae
Guuz1 <guuzoguz@live.fr>
2018-02-08 16:46:07 +0100

    Ajout Test redirection erreurs
    Redirections quand il y a une erreurs

---
+++ my_project/src/AppBundle/Service/ExampleService.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gaelvandecasteele
+ * Date: 08/02/2018
+ * Time: 15:14
+ */
+
+namespace AppBundle\Service;
+
+class ExampleService
+{
+    /**
+     * @param string $controller
+     * @return bool
+     */
+    public function has($controller)
+    {
+        list($class, $action) = explode('::', $controller, 2);
+        return class_exists($class);
+    }
+}
